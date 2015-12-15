bb5cf5d613f7b4bce48c530c1361d7fdef13aa5f
James Stormes <jstormes@stormes.net>
2015-12-15 11:22:05 -0600

    Asset Manager working

--- config/module.config.php
+++ config/module.config.php
@@ -1,6 +1,14 @@
 <?php
 return array(
     
+    // Public our assets like JavaScript files and CSS files.
+    'asset_manager' => array(
+        'resolver_configs' => array(
+            'paths' => array(
+                __DIR__ . '/../public',
+            ),
+        ),
+    ),
     
     
 );
